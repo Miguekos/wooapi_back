c4688f35cf5632fefc4266be24d260b9135df868
Kevin Silva Malca <kevin.silva.095@gmail.com>
2020-05-10 19:48:36 -0500

    ServicioOlva

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.secret_key = "secret key"
+
+# logging.basicConfig(
+#         handlers=[RotatingFileHandler('./logs/wooadmin_log.log', maxBytes=10000000, backupCount=10)],
+#         level=logging.DEBUG,
+#         format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+#         datefmt='%Y-%m-%dT%H:%M:%S')
+
+import controllers.integraciones.olva
---
+++ controllers/integraciones/olva.py
@@ -0,0 +1,52 @@
+from app import app
+from flask import jsonify, flash, request
+import logging
+import requests
+import json
+
+
+@app.route('/olva', methods=['POST'])
+def enviOlva():
+    _json = request.json
+    try:
+        # Cuerpo de Olva
+        body_json = {
+            "consignado": _json['consignado'],
+            "nroDocConsignado": _json['nroDocConsignado'],
+            "direccion": _json['direccion'],
+            "ubigeo": _json['ubigeo'],
+            "codigoRastreo": _json['codigoRastreo'],
+            "observacion": _json['observacion'],
+            "montoArticulo": _json['montoArticulo'],
+            "receptor": _json['receptor'],
+            "rucSeller": _json['rucSeller'],
+            "ubigeoSeller": _json['ubigeoSeller'],
+            "seller": _json['seller'],
+            "direccionSeller": _json['direccionSeller'],
+            "contacto": _json['contacto'],
+            "telefono": _json['telefono'],
+            "codClienteRucDni": _json['codClienteRucDni'],
+            "total": _json['total'],
+            "formaPago": _json['formaPago'],
+            "tipoEnvio": _json['tipoEnvio'],
+            "altoEnvio": _json['altoEnvio'],
+            "anchoEnvio": _json['anchoEnvio'],
+            "largoEnvio": _json['largoEnvio'],
+            "pesoUnitario": _json['pesoUnitario'],
+            "codContenedor": _json['codContenedor']
+        }
+
+        # Servicio de Olva
+        headersOlva = {'Content-Type': 'application/json'}
+        response = requests.put('http://200.31.99.175:8080/RegistroRemito-1.0-SNAPSHOT/webresources/remito',
+                                data=json.dumps(body_json), headers=headersOlva)
+        rs = response.json()
+        if rs['lista'] == []:
+            return rs
+        else:
+            jsonError = {
+                "jsonError": rs['lista']
+            }
+            return jsonify(jsonError)
+    except ValueError:
+        print(ValueError)
