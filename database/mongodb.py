c5544ed245d243e54515a3333ff2d3b0615e7d14
miguel.rodriguez <miguel.rodriguez@acceso.com.pe>
2022-02-15 08:57:15 -0500

    add produccion

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,19 +1,19 @@
 import pymongo
-# myclient = pymongo.MongoClient("mongodb://95.111.235.214:32773")
-myclient = pymongo.MongoClient("mongodb://95.111.235.214:32768")
+myclient = pymongo.MongoClient("mongodb://95.111.235.214:32773")
+# myclient = pymongo.MongoClient("mongodb://95.111.235.214:32768")
 
 # myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 # app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/cuiappte"
 
 mydb = myclient["woocomerce"]
-mycol = mydb["envios_olva"]
+mycol = mydb["envios_rapunzel"]
 
 
 mydbnew = myclient["envios"]
 mycolnew = mydbnew["envios_express"]
 
 
-mytexdev = myclient["tuenvioexpress_dev"]
+mytexdev = myclient["tuenvioexpress"]
 mytexdevcomunas = mytexdev["comunas"]
 
 
